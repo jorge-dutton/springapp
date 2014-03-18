161b635362175fedc6bf0900cad3cf1cb37bda86
Jorge Dutton <jorge.dutton@jorgedutton-PC>
2014-03-18 20:36:59 +0100

    Cambio a Double en atributo price

--- src/main/java/springapp/domain/Product.java
+++ src/main/java/springapp/domain/Product.java
@@ -7,7 +7,7 @@ public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String description;
-	private double price;
+	private Double price;
 	
 	public String getDescription() {
 		return description;
@@ -15,15 +15,15 @@ public class Product implements Serializable {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public double getPrice() {
+	public Double getPrice() {
 		return price;
 	}
-	public void setPrice(double price) {
+	public void setPrice(Double price) {
 		this.price = price;
 	}
 	
 	public String toString() {
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		buffer.append("Description: " + this.description + "; ");
 		buffer.append("Price:       " + this.price);
 		return buffer.toString();
